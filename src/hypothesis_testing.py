1f3b77c9724d03902ef0e1cae6c893f36c256264
Daniel Faltynowski <danielfaltynowski@MacBook-Pro-Daniel.local>
2025-06-26 00:58:12 +0200

    Hypothesis Testing: T-Statistics for One Sample, Independent Samples, Dependent Samples

--- src/hypothesis_testing.py
+++ src/hypothesis_testing.py
@@ -1,3 +1,6 @@
+from src.descriptive_statistics import mean, variance, standard_deviation
+
+
 def benjamini_hockberg_method(p_values: list[float]) -> list[float]:
     for p_value in p_values:
         if not isinstance(p_value, (int, float)) or not 0 <= p_value <= 1:
@@ -14,3 +17,20 @@ def benjamini_hockberg_method(p_values: list[float]) -> list[float]:
             reduced_p_values[i] = reduced_p_values[i + 1]
         i = i - 1
     return reduced_p_values
+
+
+def one_sample_t_statistic(sample: list[float], population_mean: float) -> float:
+    length = len(sample)
+    return ( mean(data = sample) - population_mean ) / ( standard_deviation(data = sample) / ( length ** 0.5 ) )
+
+
+def independent_samples_t_statistics(sample_one: list[float], sample_two: list[float]) -> float:
+    length_one = len(sample_one)
+    length_two = len(sample_two)
+    return ( mean(sample_one) - mean(sample_two) ) / \
+           ( ( variance(sample_one) / length_one + variance(sample_two) / length_two ) ** 0.5 )
+
+
+def dependent_samples_t_statistic(differences: list[float]) -> float:
+    length = len(differences)
+    return ( mean(data = differences) - 0 ) / ( standard_deviation(data = differences) / ( length ** 0.5 ) )
